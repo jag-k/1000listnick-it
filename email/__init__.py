c44c3a1dc9d90f1c552cc220a7162e17fee25ba0
jagk <jag-k58@ya.ru>
2020-05-11 14:25:00 +0300

    :sparkles: Added email

--- email/__init__.py
+++ email/__init__.py
@@ -1,3 +1,4 @@
+from color_class import Color
 from email.db import *
 
 
@@ -9,11 +10,63 @@ def add_to_queue(email: str, title: str = "", content: str = "", as_html: bool =
             content=content,
             as_html=as_html,
         )
-        print(n)
         commit()
-        Message.select().show()
+        # Message.select().show()
 
 
-def send(*emails: str):
+def send(*emails: str, real_send: bool = False):
     if not emails:
-        pass
+        filter_email = lambda x: x.status == "wait"
+    else:
+        filter_email = lambda x: x.status == "wait" and x.email in emails
+
+    n = 0
+    with db_session:
+        for m in Message.select(filter_email):
+            if real_send:
+                m.status = "ok"  # TODO: Сделать реальную отправку по EMAIL.
+            else:
+                m.status = "ok"
+            n += 1
+    return n
+
+
+def check(*emails, email_count: int = -1, status: str = None):
+    if status == "all":
+        if not emails:
+            filter_email = lambda x: True
+        else:
+            filter_email = lambda x: x.email in emails
+    elif status is not None:
+        if not emails:
+            filter_email = lambda x: x.status == status
+        else:
+            filter_email = lambda x: x.status == status and x.email in emails
+
+    with db_session:
+        s = Message.select(filter_email)
+        if email_count > 0:
+            s = s[:email_count]
+        s = list(s)
+        if not s:
+            print("Сообщений в очереди не найдено...")
+        else:
+            print("Найдено %s сообщений:" % Color(str(len(s)), Color.THUMBNAIL, Color.GREEN), end="")
+        for m in s:  # type: Message
+            print("""
+Почта: %s
+Статус отправки: %s
+Заголовок: %s
+Это HTML?: %s
+Контент: %s""" % (
+                Color(m.email, Color.THUMBNAIL, Color.UNDERLINE, Color.BLUE),
+                Color(m.status.upper(), {
+                    "ok": Color.GREEN,
+                    "err": Color.RED,
+                    "wait": Color.BLUE
+                }.get(m.status, 0), Color.THUMBNAIL),
+                Color(m.title, Color.THUMBNAIL),
+                Color("Да", Color.GREEN) if m.as_html else Color("Нет", Color.RED),
+                Color(m.content, Color.THUMBNAIL),
+            ))
+
--- email/__main__.py
+++ email/__main__.py
@@ -6,6 +6,11 @@ from email import *
 parser = argparse.ArgumentParser(
     description="Задание на реализацию очереди"
 )
+parser.add_argument(
+    "-n", "--no-color", action="store_true", default=False,
+    help="Убрать цветной вывод"
+)
+
 subparser = parser.add_subparsers(help="Возможные команды")
 
 # === ADD COMMAND ===
@@ -44,6 +49,10 @@ send_queue.add_argument(
     "-a", "--all", action="store_true", default=False,
     help="Отправить ВСЕ сообщения из очереди (игнорирует введённые адреса)"
 )
+send_queue.add_argument(
+    "-r", "--real-send", action="store_true", default=False,
+    help="Совершить РЕАЛЬНУЮ отправку на почты (пока не реализованно)"
+)
 
 # === CHECK COMMAND ===
 
@@ -53,8 +62,8 @@ check_queue.add_argument(
     help="Показывает историю сообщений на данные email адреса (по умолчанию все)"
 )
 check_queue.add_argument(
-    "-s", "--status", action="store", type=str, choices=("ok", "err", "wait"), default="wait",
-    help="Показывает сообщения в очереди (wait), отправленные (ok) и с ошибкой (err)"
+    "-s", "--status", action="store", type=str, choices=("all", "ok", "err", "wait"), default="all",
+    help="Показывает сообщения в очереди (wait), отправленные (ok), с ошибкой (err) и все сообщения (all)"
 )
 check_queue.add_argument(
     "-c", "--count", action="store", type=int, default=-1,
@@ -63,12 +72,13 @@ check_queue.add_argument(
 
 argv = parser.parse_args()
 
-print(argv)
+# print(argv)
+if argv.no_color:
+    Color.DE_COLOR = True
 
 
 def main():
-    if "email" in argv:
-        print("add")
+    if "email" in argv:  # ADD
         as_html = False
         if argv.html:
             with argv.html as file:
@@ -86,15 +96,16 @@ def main():
             content=content,
             as_html=as_html,
         )
+        print("Сообщение для %s добавлено в очередь!" % Color(argv.email, Color.THUMBNAIL, Color.UNDERLINE, Color.BLUE))
 
-    elif "all" in argv:
-        print("send")
+    elif "all" in argv:  # SEND
+        send_count = send(*(argv.emails if not argv.all else []), real_send=argv.real_send)
+        print("Отправлено %s сообщений" % Color(send_count, Color.THUMBNAIL, Color.GREEN))
 
-    elif "status" in argv:
-        print("check")
+    elif "status" in argv:  # CHECK
+        check(*argv.emails, email_count=argv.count, status=argv.status)
 
-    else:
-        print("no command")
+    else:  # NO COMMAND
         parser.print_help()
 
 
