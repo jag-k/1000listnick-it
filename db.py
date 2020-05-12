06a98aaf53bf3763b5c60eae70884168c4706b5e
jagk <jag-k58@ya.ru>
2020-05-12 19:16:29 +0300

    :sparkles: init to Vercel ▲Now

---
+++ db.py
@@ -0,0 +1,23 @@
+from json import load
+
+from pony.orm import *
+
+db = Database()
+db.bind(**load(open('database_settings.json')))
+
+
+class URL(db.Entity):
+    original = PrimaryKey(str)
+    short = Required(str, unique=True)
+
+
+db.generate_mapping(create_tables=True)
+
+
+if __name__ == '__main__':
+    with db_session:
+        URL.select().show()
+        # URL(
+        #     original="https://yandex.ru",
+        #     short="ya"
+        # )
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Данный этап ещё не готов...")
+from sanic import Sanic
+from sanic.request import Request
+from sanic.response import json
+
+from db import *
+
+app = Sanic(__name__)
+
+
+@app.route('/')
+def main_r(req: Request):
+    with db_session:
+        return json(URL.select().first().to_dict())
+
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=8080,
+    )
