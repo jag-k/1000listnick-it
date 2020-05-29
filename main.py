2c8f0bf972cdef55b5b372005233f81870a59603
jagk <jag-k58@ya.ru>
2020-05-30 01:24:30 +0300

    :card_file_box: update db

--- main.py
+++ main.py
@@ -2,23 +2,20 @@ from sanic import Sanic
 from sanic.request import Request
 from sanic.response import json
 
-from extensions import *
-# from db import *
+from db import *
 
 app = Sanic(__name__)
-
-app.blueprint(calendar, url_prefix="/calendar")
 
 
 @app.route('/')
 def main_r(req: Request):
-    return json(
-        list(map(lambda route: route.uri, calendar.routes))
-    )
+    with db_session:
+        return json(URL.select().first().to_dict())
 
 
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
         port=8080,
+        auto_reload=True,
     )
